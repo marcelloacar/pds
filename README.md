6279db8af2ea24a8a6e6c41e04225ccf43a4d132
Marcello Acar <marcello.acar@gmail.com>
2019-09-18 20:57:52 -0300

    ädding angular bulma framewotrk

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Pds
+# AngularBulma
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.5.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MenusComponent } from './menus/menus.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MenusComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgbModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
