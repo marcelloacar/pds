487b6621cacc8054814892a0455a2b7487fda099
cip.matheus.crivella <DzXYsF2Wh_z9UF9rFWss>
2019-09-19 16:36:57 -0300

    create menus and tiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MenusComponent } from './menus/menus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenusComponent
   ],
   imports: [
     BrowserModule
