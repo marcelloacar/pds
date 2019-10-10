4d087a9f248ef7706479dae464a6a5bccdb25c50
Luiz Martins <luiz@luizcoder.com.br>
2019-10-10 11:30:19 -0300

    Add wp-config on repo and redirect wp-admin to admin

--- wp-content/themes/shopper/functions.php
+++ wp-content/themes/shopper/functions.php
@@ -53,3 +53,11 @@ if ( shopper_is_woocommerce_activated() ) {
 	require_once 'inc/woocommerce/shopper-wc-template-hooks.php';
 	require_once 'inc/woocommerce/shopper-wc-template-functions.php';
 }
+
+add_filter('site_url',  'wpadmin_filter', 10, 3);
+ function wpadmin_filter( $url, $path, $orig_scheme ) {
+  $old  = array( "/(wp-admin)/");
+  $admin_dir = WP_ADMIN_DIR;
+  $new  = array($admin_dir);
+  return preg_replace( $old, $new, $url, 1);
+ }
