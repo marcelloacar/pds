91d28a97186611338ec13a0e8cb077374fcbbe85
Luiz Martins <luiz@luizcoder.com.br>
2019-11-07 20:38:39 -0300

    Adjust customer table

--- database/migrations/2014_10_12_000000_create_customers_table.php
+++ database/migrations/2014_10_12_000000_create_customers_table.php
@@ -17,7 +17,7 @@ class CreateCustomersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->integer('status')->default(1);
             $table->softDeletes();
             $table->rememberToken();
